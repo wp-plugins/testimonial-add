8e6e6b71ebf69b95ead7b0171c2ffe2e9c7a897e
umarbajwa <umarbajwa@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-07 13:31:00 +0000

    Bug Fix
    git-svn-id: https://plugins.svn.wordpress.org/testimonial-add/tags/2.0@1214878 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tss_includes.php
+++ tss_includes.php
@@ -5,7 +5,7 @@ include 'mpsp_metaboxes.php';
 include 'mpsp_scripts.php';
 include 'mpsp_shortcode_gen.php';
 include 'tss_admin_pages.php';
-include 'TssFrontpageUi.php';
+include 'TssFrontpageUI.php';
 include 'tss_front_end_form.php';
 
 
